d16ad85f9d19c683801d1bf07bd6814d77239705
Sako <78461130+Sakooooo@users.noreply.github.com>
2023-11-03 22:35:08 +0400

    (feat) add placeholder logo and funny text thing

--- App.jsx
+++ App.jsx
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { useRef } from 'react'
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Image
-          style={styles.logo}
-          source={require('./assets/icon.png')}/>
-      </View>
+      <Image
+        source={require('./assets/icon.png')}
+        style={styles.startup}
+      />
+      <Text style={styles.funnytext}>I would have been much more stable in Dart!</Text>
     </View>
   );
 }
@@ -17,16 +23,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
       backgroundColor: '#444444',
-    //alignItems: 'center',
-    //justifyContent: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
   },
   header: {
       backgroundColor: 'black',
       opacity: 0.1,
       padding: 40,
   },
-    logo: {
-        width: 50,
-        height: 50,
+    startup: {
+        width: 100,
+        height: 100,
+    },
+    funnytext: {
+        paddingTop: 30,
+        fontSize: 17,
+        color: 'white',
     },
 });
